c73ad603e8ad22739bb22b9dba9f594255af08fe
Kevin <kevinabram1000@gmail.com>
2019-04-18 16:38:42 +0700

    Added a new string variable named variable

--- LittleHappy/ViewController.swift
+++ LittleHappy/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //I added a new variable
+        var variable = ""
     }
 
 
