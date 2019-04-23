7f41089c7ab67de7d1a230f01ba20397db3f52dd
NandaMochammad <nanda.mocha@gmail.com>
2019-04-23 20:28:49 +0700

    Finish Documentation Finish Percentage Finish Section Header

--- LittleHappy/Controller/DiaryViewController.swift
+++ LittleHappy/Controller/DiaryViewController.swift
@@ -44,110 +44,79 @@ class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        diaryTableView.reloadData()
-        
-        //Get Data from UserDefaults
-        //Membuat User Default untuk menyimpan data array
-        
+        //Load Data from Data ManagerUserDefaults
         DataManager.shared.loadDataFromUserDefaults()
         
         //Declare the table view to be a delegate for UITableViewDelegate, UITableViewDataSource
         diaryTableView.delegate = self
         diaryTableView.dataSource = self
         
-        //Register your MessageCell.xib file here:
+        //Register MessageCell.xib file here:
         diaryTableView.register(UINib(nibName: "diaryCustomCell", bundle: nil), forCellReuseIdentifier: "diaryCustomCell")
         
-        if let a : [String] = DataManager.shared.dateOfFeels.reversed(){
-            dateFeelsArray = a
-        }
+        sectionConfig()
+        feelingPercentage()
+    }
+    
+    //MARK: - SET SECTION
+    //Set Section Config
+    func sectionConfig(){
+        //set Reversed array, Latest to Oldest
+        dateFeelsArray = DataManager.shared.dateOfFeels.reversed()
         
+        //Loop for define the section changed location
         for j in 1 ... dateFeelsArray.count - 1{
             if dateFeelsArray[j] != dateFeelsArray[j-1]{
                 arrayOfIndex.append(j)
                 totalSection += 1
-            }
-//            print(dateFeelsArray[j], totalSection, j)
-        }
-//        print(arrayOfIndex)
-//        print(dateFeelsArray)
-        
-        
-        feelingPercentage()
-    }
+            }//if
+        }//for
+    }//func
     
-    //SET CELL DIARY
-    //Method hasil inherit UITableViewDelegate, UITableViewDataSource
     
+    //MARK: - SET CELL
+    //Declare cellForRowAt Content Based on Section
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let diaryKindOfFeels : [String] = DataManager.shared.kindOfFeels.reversed()
         let diaryDateOfFeels : [String] = DataManager.shared.dateOfFeels.reversed()
         let diaryTimeOfFeels : [String] = DataManager.shared.timeOfFeels.reversed()
         
-//        if diaryDateOfFeels[indexPath.row] == bc{
             let cell = tableView.dequeueReusableCell(withIdentifier: "diaryCustomCell", for: indexPath) as! DiaryCustomTableViewCell
-//        var a = arrayOfIndex [1] - arrayOfIndex[0]
-//
-//        if indexPath.section != totalSection - 1{
-////            a =
-//        }
-//        if indexPath.section == 0 {
-//            a = 0
+
             cell.feelDiary.text = diaryKindOfFeels[indexPath.row+arrayOfIndex[indexPath.section]]
             cell.dateDiary.text = diaryDateOfFeels[indexPath.row+arrayOfIndex[indexPath.section]]
             cell.timeDiary.text = diaryTimeOfFeels[indexPath.row+arrayOfIndex[indexPath.section]]
             cell.iconDiary.image = UIImage(named: cell.feelDiary.text ?? "Happy")
-//        }else{
-//            cell.feelDiary.text = diaryKindOfFeels[indexPath.row+a]
-//            cell.dateDiary.text = diaryDateOfFeels[indexPath.row+a]
-//            cell.timeDiary.text = diaryTimeOfFeels[indexPath.row+a]
-//            cell.iconDiary.image = UIImage(named: cell.feelDiary.text ?? "Happy")
-//        }
-       // for i in 0 ... indexPath.section {
+
             print("section ",indexPath.section)
-       // }
-        
-        
-//        var newView = UIView(frame: CGRectMake(200, 10, 100, 50))
-//        cell.contentView.addSubview(newView)
-        
+
             return cell
-
-//        }
-//            else{
-//            diaryTableView.register(UINib(nibName: "dateCustomCell", bundle: nil), forCellReuseIdentifier: "dateCustomCell")
-//
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCustomCell", for: indexPath) as! DateCustomTableViewCell
-//            cell.dateLabel.text = diaryDateOfFeels[indexPath.row]
-//
-//            bc = diaryDateOfFeels[indexPath.row]
-//
-//            return cell
-//        }
-    }
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return totalSection
-    }
+    }//func
     
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return dateFeelsArray[arrayOfIndex[section]]
-        
-    }
+    //Declare numberOfRowsInSection -> What Index will be shown
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
+        
         if section != totalSection - 1{
             print("numSection \(section)",arrayOfIndex [section+1] - arrayOfIndex[section])
             return arrayOfIndex [section+1] - arrayOfIndex[section]
         }else{
             print("numSection \(section)",dateFeelsArray.count-arrayOfIndex[section])
             return dateFeelsArray.count-arrayOfIndex[section]
-        }
-        
-        
-    }
+        }//else
+    }//func
     
+    //Declare numberOfSection Based on total section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return totalSection
+    }//func
+    
+    //Declare titleForHeaderInSection
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return dateFeelsArray[arrayOfIndex[section]]
+    }//func
+    
+    //Give an Action when button tapped -> Fade To White
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //Give an Action when button tapped
         tableView.deselectRow(at: indexPath, animated: true)
 
     }
@@ -158,18 +127,19 @@ class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDat
         diaryTableView.rowHeight = UITableView.automaticDimension
 
     }
-
     
-    //Set Feeling Percentage
+    
+    //MARK: - SET PERCENTAGE
     func feelingPercentage(){
         var feelingTotal : [Double] = [0,0,0,0,0]
         let total : Double = Double(DataManager.shared.kindOfFeels.count)
         let views : [UIView] = [vHappy, vSad, vAngry, vFear, vDisgust]
         let icons : [UIImageView] = [happyIcon, sadIcon, angryIcon, fearIcon, disgustIcon]
         
-        let a = DataManager.shared.kindOfFeels
+        let feels = DataManager.shared.kindOfFeels
         
-        for i in a {
+        //Get Value each feel
+        for i in feels {
             if i == "Happy"{
                 feelingTotal[0] += 1
             }else if i == "Sad"{
@@ -183,6 +153,7 @@ class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDat
             }
         }
         
+        //Get percentage each feel
         for i in 0 ... feelingTotal.count - 1{
             feelingTotal[i] = feelingTotal[i] / total * 100
             feelingTotal[i] = feelingTotal[i].rounded(.up)
@@ -194,16 +165,14 @@ class DiaryViewController: UIViewController, UITableViewDelegate, UITableViewDat
         fearPercentage.text = "\(Int(feelingTotal[3]))%"
         disgustPercentage.text = "\(Int(feelingTotal[4]))%"
         
-//        print(feelingTotal)
-        
+        //Set Size Each Feels
         for i in 0 ... views.count - 1{
             if feelingTotal[i] != 0{
             icons[i].bounds.size = CGSize(width: feelingTotal[i]*2+10, height: feelingTotal[i]*2+10)
-            }
-//            else{
-//                icons[i].bounds.size = CGSize(width: 10, height: 10)
-//            }//Else
+            }//else
         }//For
     }//Func
+    
+    
 }//Class
 
