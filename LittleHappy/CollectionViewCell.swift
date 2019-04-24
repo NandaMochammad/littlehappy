a902744ac1374bf3124124c0a312c80d08337233
iptkrisna <krisna1616@gmail.com>
2019-04-24 20:00:09 +0700

    ubah total flow layout program

---
+++ LittleHappy/CollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewCell.swift
+//  LittleHappy
+//
+//  Created by I Putu Krisna on 24/04/19.
+//  Copyright © 2019 littlehappy. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        DispatchQueue.main.async {
+            self.cellView.layer.shadowColor = UIColor.gray.cgColor
+            self.cellView.layer.shadowOpacity = 0.5
+            self.cellView.layer.shadowOpacity = 10
+            self.cellView.layer.shadowOffset = .zero
+            self.cellView.layer.shadowPath = UIBezierPath(rect: self.cellView.bounds).cgPath
+            self.cellView.layer.shouldRasterize = true
+        }
+    }
+
+}
---
+++ LittleHappy/FamilyViewController.swift
@@ -0,0 +1,58 @@
+//
+//  FamilyViewController.swift
+//  familyHappy
+//
+//  Created by I Putu Krisna on 24/04/19.
+//  Copyright © 2019 Kulit Biru YG. All rights reserved.
+//
+
+import UIKit
+
+class FamilyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    let familyArray = [Person.me, Person.father, Person.mother, Person.brother, Person.sister]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        collectionView.register(UINib.init(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "collectionCell")
+        let flowLayout = UPCarouselFlowLayout()
+        flowLayout.itemSize = CGSize(width: UIScreen.main.bounds.size.width - 500, height: collectionView.frame.size.height)
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sideItemScale = 0.8
+        flowLayout.sideItemAlpha = 1.0
+        flowLayout.spacingMode = .fixed(spacing: 5.0)
+        collectionView.collectionViewLayout = flowLayout
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return familyArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionViewCell
+        
+        cell.cellImage.image = UIImage(named: familyArray[indexPath.row].description.lowercased())
+        cell.cellLabel.text = familyArray[indexPath.row].description
+        //        cell.cellImage.image =
+        
+        return cell
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
