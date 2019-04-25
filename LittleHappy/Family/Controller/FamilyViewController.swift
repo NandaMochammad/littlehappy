f3aca0f6c8848f098a7962a71d61e2575ff59f50
William Sjahrial <will@willsjah.com>
2019-04-25 16:27:55 +0700

    Add Krisna's part

---
+++ LittleHappy/Family/Controller/FamilyViewController.swift
@@ -0,0 +1,137 @@
+//
+//  FamilyViewController.swift
+//  familyHappy
+//
+//  Created by I Putu Krisna on 24/04/19.
+//  Copyright © 2019 Kulit Biru YG. All rights reserved.
+//
+
+import UIKit
+
+class FamilyViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    let familyArray = [Person.me, Person.father, Person.mother, Person.brother, Person.sister]
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        let flowLayout = UPCarouselFlowLayout()
+        
+        UIDevice.current.setValue(value, forKey: "orientation")
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        
+        flowLayout.itemSize = CGSize(width: UIScreen.main.bounds.size.width - 400, height: collectionView.frame.size.height)
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.sideItemScale = 0.8
+        flowLayout.sideItemAlpha = 1.0
+        flowLayout.spacingMode = .fixed(spacing: 20.0)
+        
+        collectionView.collectionViewLayout = flowLayout
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        //        print(UIScreen.main.bounds.size.width - 400)
+        //        print(collectionView.frame.size.height)
+        
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+    
+    override var shouldAutorotate: Bool {        
+        return UIDevice.current.orientation != .portrait
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return familyArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionViewCell
+        
+        cell.cellImage.image = UIImage(named: familyArray[indexPath.row].description.lowercased())
+        cell.cellLabel.text = familyArray[indexPath.row].description
+        cell.layer.cornerRadius = 120
+        cell.cellButton.tag = indexPath.row
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            cell.cellImage.frame.origin.y -= 20
+            //            cell.cellImage.transform = CGAffineTransform.init(rotationAngle: 45)
+            
+        }){_ in
+            UIView.animateKeyframes(withDuration: 0.5, delay: 0.25, options: [.autoreverse, .repeat], animations: {
+                cell.cellImage.frame.origin.y += 20
+                //                cell.cellImage.transform = CGAffineTransform.init(rotationAngle: -45)
+            }, completion: nil)
+        } 
+        
+        return cell
+        
+    }
+    
+    @IBAction func cellButton(_ sender: UIButton) {
+        
+        print("cellButton \(sender.tag)")
+        
+        let person = familyArray[sender.tag]
+        
+        performSegue(withIdentifier: "PhotosSegue", sender: person)
+        
+        
+        
+        //        UIView.animate(withDuration: 1, animations: {
+        //            cell.cellImage.frame.origin.y -= 20
+        //            //            cell.cellImage.transform = CGAffineTransform.init(rotationAngle: 45)
+        //
+        //        }){_ in
+        //            UIView.animateKeyframes(withDuration: 1, delay: 0.25, options: [.autoreverse, .repeat], animations: {
+        //                cell.cellImage.frame.origin.y += 20
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "PhotosSegue" {
+            
+            let vc = segue.destination as! PhotosViewController
+            let person = sender as! Person
+            vc.currPerson = person
+        }
+        
+    }
+    
+}
+/*
+extension UINavigationController {
+    
+    override open var shouldAutorotate: Bool {
+        get {
+            if let visibleVC = visibleViewController {
+                return visibleVC.shouldAutorotate
+            }
+            return super.shouldAutorotate
+        }
+    }
+    
+    override open var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation{
+        get {
+            if let visibleVC = visibleViewController {
+                return visibleVC.preferredInterfaceOrientationForPresentation
+            }
+            return super.preferredInterfaceOrientationForPresentation
+        }
+    }
+    
+    override open var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        get {
+            if let visibleVC = visibleViewController {
+                return visibleVC.supportedInterfaceOrientations
+            }
+            return super.supportedInterfaceOrientations
+        }
+    }
+ }
+*/
---
+++ LittleHappy/Family/Model/CollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  CollectionVC.swift
+//  LittleHappy
+//
+//  Created by I Putu Krisna on 24/04/19.
+//  Copyright © 2019 littlehappy. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        DispatchQueue.main.async {
+            self.layer.shadowColor = UIColor.lightGray.cgColor
+            self.layer.shadowOpacity = 0.5
+            self.layer.shadowOpacity = 10
+            self.layer.shadowOffset = .zero
+            self.layer.shadowPath = UIBezierPath(rect: self.layer.bounds).cgPath
+            self.layer.shouldRasterize = true
+            
+        }
+    }
+}
