80304a9ed67b6862eb523dd08f9841146923c7ec
William Sjahrial <will@willsjah.com>
2019-04-25 17:53:21 +0700

    Add Kevin's onboarding part

---
+++ LittleHappy/Onboarding/BoyorGirl_ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  BoyorGirl_ViewController.swift
+//  LittleHappy
+//
+//  Created by Kevin Abram on 25/04/19.
+//  Copyright © 2019 littlehappy. All rights reserved.
+//
+
+import UIKit
+
+class BoyorGirl_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Boy_Click(_ sender: UIButton) {
+        //If boy is clicked, set the data of user defaults to boy
+        UserDefaults.standard.set("Boy", forKey: "user_first_time")
+        performSegue(withIdentifier: "DailyFeelSegue", sender: nil)
+    }
+    
+    @IBAction func Girl_Click(_ sender: UIButton) {
+        //If girl is clicked, set the data of user defaults to girl
+        UserDefaults.standard.set("Girl", forKey: "user_first_time")
+        performSegue(withIdentifier: "DailyFeelSegue", sender: nil)
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LittleHappy/Onboarding/Onboarding_Controller_1.swift
@@ -0,0 +1,55 @@
+//
+//  Onboarding_Controller_1.swift
+//  LittleHappy
+//
+//  Created by Kevin Abram on 24/04/19.
+//  Copyright © 2019 littlehappy. All rights reserved.
+//
+
+import UIKit
+
+class Onboarding_Controller_1: UIViewController {
+    
+    @IBOutlet weak var touchToStartLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Do the basic appear and disappear animation
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [.repeat, .autoreverse], animations: {
+            self.touchToStartLabel.alpha = 0
+        }, completion: nil)
+        
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @IBAction func skip_onboarding(_ sender: UIButton) {
+        
+        //Prints the value of the app
+        let name = UserDefaults.standard.string(forKey: "user_first_time")
+        
+        //This checks the name, and do condition if the onboarding should be skipped.
+        if name == nil
+        {
+            // Do the segue to the onboarding here
+        }
+        else
+        {
+            //Do the segue to skip onboarding here
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
