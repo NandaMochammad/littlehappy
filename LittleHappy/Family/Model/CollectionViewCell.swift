e3cf56178a2dae9c2711c2f2b2f018255223bfdc
iptkrisna <krisna1616@gmail.com>
2019-04-27 20:00:12 +0700

    delete dispatch method in collection view

--- LittleHappy/Family/Model/CollectionViewCell.swift
+++ LittleHappy/Family/Model/CollectionViewCell.swift
@@ -18,14 +18,11 @@ class CollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        DispatchQueue.main.async {
-            self.layer.shadowColor = UIColor.lightGray.cgColor
-            self.layer.shadowOpacity = 0.5
-            self.layer.shadowOpacity = 10
-            self.layer.shadowOffset = .zero
-            self.layer.shadowPath = UIBezierPath(rect: self.layer.bounds).cgPath
-            self.layer.shouldRasterize = true
-            
-        }
+        self.layer.shadowColor = UIColor.lightGray.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowOpacity = 10
+        self.layer.shadowOffset = .zero
+        self.layer.shadowPath = UIBezierPath(rect: self.layer.bounds).cgPath
+        self.layer.shouldRasterize = true
     }
 }
