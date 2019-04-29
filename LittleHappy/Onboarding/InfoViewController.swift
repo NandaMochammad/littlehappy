378ab099ea9e5b19d291702b98c8929745b6005a
Kevin <kevinabram1000@gmail.com>
2019-04-29 18:35:34 +0700

    Fixed the portrait of the info to landscape

---
+++ LittleHappy/Onboarding/InfoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InfoViewController.swift
+//  LittleHappy
+//
+//  Created by Kevin Abram on 29/04/19.
+//  Copyright © 2019 littlehappy. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension InfoViewController {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscape
+    }
+    override var shouldAutorotate: Bool {
+        return UIDevice.current.orientation != .portrait
+    }
+}
