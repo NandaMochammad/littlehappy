77fe9f98fe8716078be8a19208eb6d3918fb3f25
Kevin <kevinabram1000@gmail.com>
2019-04-29 19:43:41 +0700

    Created spacing between the text of little happy, fixed the design as well

--- LittleHappy/Onboarding/Onboarding_Controller_1.swift
+++ LittleHappy/Onboarding/Onboarding_Controller_1.swift
@@ -17,13 +17,15 @@ class Onboarding_Controller_1: UIViewController, AVAudioPlayerDelegate {
     
     @IBOutlet weak var sunPath: UIImageView!
     
-    @IBOutlet weak var littleHappyText: UIButton!
+    @IBOutlet weak var littleHappyText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         DataManager.shared.loadDataFromUserDefaults()
 
         print("Data Saved \n", DataManager.shared.kindOfFeels,"\n",DataManager.shared.dateOfFeels, "\n", DataManager.shared.timeOfFeels)
+        
+        littleHappyText.attributedText = NSAttributedString(string: "LITTLE HAPPY" ,attributes:[ NSAttributedString.Key.kern: 10])
         
         playSound()
 
